9f5a3a44e20e7f4b5c1b2939219b8a2d3e4f364c
ccheung93 <ccheung@udel.edu>
2022-11-22 14:35:36 -0500

    decreased weight threshold for ikarus part of conf

--- src/conf.f90
+++ src/conf.f90
@@ -3001,7 +3001,7 @@ Contains
             strfmt = '(F9.6,5X,A)'
             wsum=0_dp
             i=0
-            Do While (wsum < 0.99 .and. i < nconfs)
+            Do While (wsum < 0.95 .and. i < nconfs)
                 i=i+1
                 Write(98, strfmt) Wsave(i,j), strcsave(i,j)
                 If (Any(wgtconfs%strconfs == strcsave(i,j))) Then
