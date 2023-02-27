7c409f568a0c00ea1734bf2d82b2db9979091e21
ccheung93 <ccheung@udel.edu>
2023-02-27 17:58:45 -0500

    added comments

--- py-lib/add.py
+++ py-lib/add.py
@@ -1,19 +1,40 @@
-from subprocess import Popen, PIPE, STDOUT, run
+from subprocess import run
+import sys
 
-add_inp = open("add.in", 'rb').read()
+# Read input to add from add.in if it exists, otherwise create it
+try:
+    open("add.in", 'rb').read()
+except:
+    f = open("add.in", "w")
+    f.write("0")
+    f.close()
 
-run(["python3", "create_add_inp.py"])
+# Run script to generate ADD.INP for even and odd configurations
+run("python3 create_add_inp.py", shell=True)
 
-run(["cp","ADDeven.INP","ADD.INP"])
-proc = Popen(['add'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-proc.communicate(input=add_inp)[0]
-run(["cp","CONF.INP","CONFeven.INP"])
+# Check to see if ADDeven.INP and ADDodd.INP were created
+try:
+    open("ADDeven.INP", 'r')
+except Exception as e:
+    print(e)
+    sys.exit()
+try:
+    open("ADDodd.INP", 'r')
+except Exception as e:
+    print(e)
+    sys.exit()
+
+# Generate CONFeven.INP from ADDeven.INP
+run("cp ADDeven.INP ADD.INP", shell=True)
+run("add < add.in", shell=True)
+run("cp CONF.INP CONFeven.INP", shell=True)
 print("CONFeven.INP created")
-proc.kill()
 
-run(["cp","ADDodd.INP","ADD.INP"])
-proc = Popen(['add'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-proc.communicate(input=add_inp)[0]
-run(["cp","CONF.INP","CONFodd.INP"])
+# Generate CONFodd.INP from ADDodd.INP
+run("cp ADDodd.INP ADD.INP", shell=True)
+run("add < add.in", shell=True)
+run("cp CONF.INP CONFodd.INP", shell=True)
 print("CONFodd.INP created")
-proc.kill()
+
+# Cleanup - remove add.in
+run("rm add.in", shell=True)
