7a44786c2851615872a43033499028916971cbb6
ccheung93 <ccheung@udel.edu>
2023-05-16 10:31:58 -0400

    minor refactoring

--- py-lib/add.py
+++ py-lib/add.py
@@ -16,17 +16,17 @@ except:
 run("python3 create_add_inp.py", shell=True)
 
 # Check if ADDeven.INP and ADDodd.INP exist
-even_exist = os.path.isfile('ADDeven.INP')
-odd_exist = os.path.isfile('ADDodd.INP')
+even_exists = os.path.isfile('ADDeven.INP')
+odd_exists = os.path.isfile('ADDodd.INP')
 
-if even_exist == True:
+if even_exists:
     # Generate CONFeven.INP from ADDeven.INP
     run("cp ADDeven.INP ADD.INP", shell=True)
     run("add_nr < add.in", shell=True)
     run("cp CONF.INP CONFeven.INP", shell=True)
     print("CONFeven.INP created")
 
-if odd_exist == True:
+if odd_exists:
     # Generate CONFodd.INP from ADDodd.INP
     run("cp ADDodd.INP ADD.INP", shell=True)
     run("add_nr < add.in", shell=True)
@@ -37,10 +37,9 @@ if odd_exist == True:
 run("rm add.in", shell=True)
 
 # Create even and odd directories
-gen_dir = re.sub('(no|No|n|N|false)', 'False', re.sub('(yes|Yes|y|Y|true)', 'True', str(input("Generate directories? "))))
+gen_dir = eval(re.sub('(no|No|n|N|false)', 'False', re.sub('(yes|Yes|y|Y|true)', 'True', str(input("Generate directories? ")))))
 
-if gen_dir == True:
-    print('here')
+if gen_dir:
     dir_path = os.getcwd()
     for dirs in ['EVEN','ODD']:
         Path(dir_path+'/'+dirs).mkdir(parents=True, exist_ok=True)
