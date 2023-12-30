48613b3ad6091327c247626aefcccd64138576f9
ccheung93 <ccheung@udel.edu>
2023-12-29 21:11:49 -0500

    separated levels into terms

--- py-lib/calc_lifetimes.py
+++ py-lib/calc_lifetimes.py
@@ -13,19 +13,22 @@ def calc_lifetimes(tr_file):
         configuration1 = line.split(',')[0]
         term1 = line.split(',')[1]
         J1 = line.split(',')[2]
+        termJ1 = term1 + J1
         configuration2 = line.split(',')[3]
         term2 = line.split(',')[4]
         J2 = line.split(',')[5]
+        termJ2 = term2 + J2
         tr_rate = float(line.split(',')[8][:-1])
+        config2 = configuration2 + ' ' + termJ2
         try:
-            tr_rates[configuration2].append(tr_rate)
+            tr_rates[config2].append(tr_rate)
         except KeyError:
-            tr_rates[configuration2] = []
-            tr_rates[configuration2].append(tr_rate)
+            tr_rates[config2] = []
+            tr_rates[config2].append(tr_rate)
     
     for config, rates in tr_rates.items():
-        lifetime = 1/sum(rates)
-        print(config, lifetime)
+        lifetime = 1/sum(rates)*1e9
+        print(config.replace('.',' '), round(lifetime,2), 'ns')
 
 if __name__ == "__main__":
     calc_lifetimes('tr_test.csv')
