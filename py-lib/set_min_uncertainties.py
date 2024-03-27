b5b65b9ef2d2b3efe8b4f7442a263bd1d2dbcfb6
ccheung93 <ccheung@udel.edu>
2024-03-27 00:28:08 -0400

    script to set minimum uncertainties to matrix elements csv

---
+++ py-lib/set_min_uncertainties.py
@@ -0,0 +1,32 @@
+
+def set_min_uncertainties(filename, min_percentage):
+    
+    f = open(filename,'r')
+    lines = f.readlines()
+    f.close()
+    
+    matrix_res = []
+    for line in lines:
+        matrix_res.append(line.replace('\n','').split(','))
+    
+    cnt = 0
+    for line in matrix_res[1:]:
+        val = float(line[6]) # matrix element value
+        unc = float(line[7]) # matrix element uncertainty
+        if val*min_percentage/100 >= unc: cnt += 1
+        line[7] = '{:,.5f}'.format(max(val*min_percentage/100, unc))
+        
+    new_filename = filename.split('.')[0] + '_adjusted.' + filename.split('.')[1]
+    print('# of matrix element uncertainties adjusted:', cnt)
+    f = open(new_filename, 'w')
+    for line in matrix_res:
+        f.write(','.join(line) + '\n')
+    f.close()
+    print(new_filename + ' has been written')
+
+if __name__ == '__main__':
+    filename = input('Input name of matrix element csv file: ')
+    min_percent = float(input('Input minimum uncertainty to set in percentage: '))
+    set_min_uncertainties(filename, min_percent)
+    
+    
