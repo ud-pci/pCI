c06a4bae002a76e44d587ae5814a3af4ce857eb3
ccheung93 <ccheung@udel.edu>
2024-07-03 11:34:59 -0400

    added AllReduceI and AllReduceR subroutines

--- src/mpi_utils.f90
+++ src/mpi_utils.f90
@@ -1,12 +1,12 @@
 Module mpi_utils    
     !
     !   Written by Jeffrey Frey, UD IT-RCI, 2/12/21
-    !   Editted by Charles Cheung, 11/10/21    
+    !   Editted by Charles Cheung, 07/03/24    
     !
-    ! This module implements MPI broadcast functions that break a buffer
+    ! This module implements MPI broadcast and allreduce functions that break a buffer
     ! that may be too large into multiple chunks.  The <stride> passed
     ! to each of the subroutines dictates how many 8-byte elements should
-    ! be passed to each underlying MPI_Bcast().
+    ! be passed to each underlying MPI_Bcast() or MPI_AllReduce().
     !
     ! If the <stride> is zero, then the environment variable CONF_BROADCAST_MAX
     ! is checked for an integer value.  If no value is found, or the value
@@ -19,7 +19,7 @@ Module mpi_utils
     
     Private
     
-    Public :: BroadcastI, BroadcastR, BroadcastD
+    Public :: BroadcastI, BroadcastR, BroadcastD, AllReduceI, AllReduceR
 
     Integer, Parameter :: MaxStride8Byte = 134217728
     
@@ -165,5 +165,95 @@ Module mpi_utils
             End If
         End If
     End Subroutine BroadcastD
-  
+
+    Subroutine AllReduceI(buffer, count, stride, op, comm, mpierr)
+        Use mpi_f08
+        Implicit None
+
+        EXTERNAL :: GetEnv
+        Type(MPI_Comm), Intent(In)                  :: comm
+        Type(MPI_Op), Intent(In)                    :: op
+        Integer(Kind=int64), Intent(In)             :: count
+        Integer, Intent(In)                         :: stride
+        Integer, Dimension(*), Intent(InOut)        :: buffer
+        Integer, Intent(InOut)                      :: mpierr
+        Character(Len=255)                          :: envvar
+        Integer(Kind=int64)                         :: count_remain, i
+        Integer                                     :: count_remain2, use_stride
+
+        If (stride .le. 0) Then
+            Call GetEnv('CONF_BROADCAST_MAX', envvar)
+            Read(envvar, '(i)') use_stride
+            If (use_stride .le. 0) use_stride = MaxStride8Byte * 2
+        Else
+            use_stride = stride
+        End If
+        If (stride .gt. MaxStride8Byte * 2) use_stride = MaxStride8Byte * 2
+        count_remain = count
+        i = 1_int64
+        mpierr = 0
+        Do While (count_remain .gt. use_stride)
+            Call MPI_AllReduce(MPI_IN_PLACE, buffer(i:i+use_stride), use_stride, MPI_INTEGER, op, comm, mpierr) 
+            If (mpierr .ne. 0 ) Then
+                Write(*,*) 'Failure reducing integer range ',i,':',i+use_stride
+                Return
+            End If
+            count_remain = count_remain - use_stride
+            i = i + use_stride
+        End Do
+        If (count_remain .gt. 0) Then
+            count_remain2 = count_remain
+            Call MPI_AllReduce(MPI_IN_PLACE, buffer(i:i+count_remain2), count_remain2, MPI_INTEGER, op, comm, mpierr) 
+            If (mpierr .ne. 0 ) Then
+                Write(*,*) 'Failure reducing integer range ',i,':',i+use_stride
+                Return
+            End If
+        End If
+    End Subroutine AllReduceI
+
+    Subroutine AllReduceR(buffer, count, stride, op, comm, mpierr)
+        Use mpi_f08
+        Implicit None
+
+        EXTERNAL :: GetEnv
+        Type(MPI_Comm), Intent(In)                     :: comm
+        Type(MPI_Op), Intent(In)                       :: op
+        Integer(Kind=int64), Intent(In)                :: count
+        Integer, Intent(In)                            :: stride
+        Real(type2_real), Dimension(*), Intent(InOut)  :: buffer
+        Integer, Intent(InOut)                         :: mpierr
+        Character(Len=255)                             :: envvar
+        Integer(Kind=int64)                            :: count_remain, i
+        Integer                                        :: count_remain2, use_stride
+
+        If (stride .le. 0) Then
+            Call GetEnv('CONF_BROADCAST_MAX', envvar)
+            Read(envvar, '(i)') use_stride
+            If (use_stride .le. 0) use_stride = MaxStride8Byte * 2
+        Else
+            use_stride = stride
+        End If
+        If (stride .gt. MaxStride8Byte * 2) use_stride = MaxStride8Byte * 2
+        count_remain = count
+        i = 1_int64
+        mpierr = 0
+        Do While (count_remain .gt. use_stride)
+            Call MPI_AllReduce(MPI_IN_PLACE, buffer(i:i+use_stride), use_stride, mpi_type2_real, op, comm, mpierr) 
+            If (mpierr .ne. 0 ) Then
+                Write(*,*) 'Failure reducing real range ',i,':',i+use_stride
+                Return
+            End If
+            count_remain = count_remain - use_stride
+            i = i + use_stride
+        End Do
+        If (count_remain .gt. 0) Then
+            count_remain2 = count_remain
+            Call MPI_AllReduce(MPI_IN_PLACE, buffer(i:i+count_remain2), count_remain2, mpi_type2_real, op, comm, mpierr) 
+            If (mpierr .ne. 0 ) Then
+                Write(*,*) 'Failure reducing real range ',i,':',i+use_stride
+                Return
+            End If
+        End If
+    End Subroutine AllReduceR
+
 End Module mpi_utils
