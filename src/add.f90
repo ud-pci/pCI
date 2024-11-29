f3abfca64773e592d03dbc9ab5bb81407227ed52
ccheung93 <ccheung@udel.edu>
2024-11-28 23:41:57 -0500

    minor refactoring for gfortran compatibility, bug fix in NOz, Ac reallocation

--- src/add.f90
+++ src/add.f90
@@ -253,7 +253,7 @@ Contains
         ! Expand the list of non-relativistic configurations into a list of relativistic configurations
         Implicit None
         Integer :: ic, icnr, kc, ji, k, n, jk, i, nx, mx, mmax1, mmax2, mmin1, m1, k1, ivar, irmax, k1max
-        Integer :: j, ir, j1, l, iq1, iq2, idif, ncr, jkmax, nozmax, num_rel_confs
+        Integer :: j, ir, j1, l, iq1, iq2, idif, ncr, jkmax, nozmax, num_rel_confs, num_rel_confs0
         Real(dp) :: qnl
         Integer, Allocatable, Dimension(:)  :: nyi, myi, nyk, myk, ivc, ni, li, iv, NozN, temp
         Integer, Allocatable, Dimension(:, :)   :: mx1, mx2, ivv
@@ -317,11 +317,12 @@ Contains
         If (.not. Allocated(mx2)) Allocate(mx2(nozmax, k1max))
 
         ! Reallocate Ac, NOz
+        num_rel_confs0 = size(NOz)
         Call move_alloc(Ac, temp2)
         Call move_alloc(NOz, temp)
         Allocate(NOz(num_rel_confs), Ac(num_rel_confs, max_num_shells))
-        NOz(1:num_rel_confs) = temp
-        Ac(1:num_rel_confs,1:max_num_shells) = temp2
+        NOz(1:num_rel_confs0) = temp
+        Ac(1:num_rel_confs0,1:max_num_shells) = temp2
         Deallocate(temp, temp2)
 
         ! Loop over non-relativistic configurations
@@ -651,7 +652,7 @@ Contains
         New=.FALSE.
   
         Do j=1,noz1                  !### check of the Pauli principle:
-            i1=sign(1.01,Ac1(j))     !#### nq.LE.(4*l+2)
+            i1=sign(1.01_dp,Ac1(j))     !#### nq.LE.(4*l+2)
             d=abs(Ac1(j))+1.E-6
             d=10*d
             nnj=d
@@ -664,7 +665,7 @@ Contains
         End Do
 
         Do j=1,noz1                  ! does this config.
-            i1=sign(1.01,Ac1(j))     !  belong to RAS?
+            i1=sign(1.01_dp,Ac1(j))     !  belong to RAS?
             d=abs(Ac1(j))+1.E-6
             d=10*d
             nnj=d
